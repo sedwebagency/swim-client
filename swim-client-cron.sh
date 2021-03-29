cd5e9b3b0c1a0652d24de9dd365638abfd043764
mircobabini@juice <sedwebagency@gmail.com>
2021-03-29 23:44:52 +0200

    cron script similar to composer installer

--- swim-client-cron.sh
+++ swim-client-cron.sh
@@ -1,1 +1,3 @@
-wget -O swim-client-installer.php 'https://raw.githubusercontent.com/sedwebagency/swim-client/master/swim-client-installer.php'; php swim-client-installer.php; rm -f swim-client-installer.php;
+php -r "copy('https://raw.githubusercontent.com/sedwebagency/swim-client/master/swim-client-installer.php', 'swim-client-setup.php');"
+php swim-client-setup.php
+php -r "unlink('swim-client-setup.php');"
