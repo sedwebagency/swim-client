6c31a9770c9375959eca5ca735bc64587aac1062
Mirco Babini <mirkolofio@gmail.com>
2021-08-21 16:46:09 +0200

    update installer: add debug

--- swim-client-installer.php
+++ swim-client-installer.php
@@ -1,5 +1,5 @@
 <?php
-define( 'SWIM_CLIENT_INSTALLER_VERSION', '1.1.0' );
+define( 'SWIM_CLIENT_INSTALLER_VERSION', '1.1.1' );
 
 // https://stackoverflow.com/a/30926828/1160173
 $home_dir        = posix_getpwuid( getmyuid() )['dir'];
@@ -25,8 +25,10 @@ $client_script = SWIM_CLIENT_PATH . '/' . SWIM_CLIENT_NAME;
 
 // check local version
 if ( file_exists( $client_script ) ) {
+	echo "client script already exists: $client_script" . PHP_EOL;
 	// maybe update?
 
+	echo "update process not implemented yet..." . PHP_EOL;
 	// todo compare version: https://developer.wordpress.org/reference/functions/get_plugin_data/
 	// todo if remote version > local version
 	// todo delete local file
@@ -34,14 +36,19 @@ if ( file_exists( $client_script ) ) {
 
 // install
 if ( ! file_exists( $client_script ) ) {
+	echo "client script does not exist: $client_script" . PHP_EOL;
+
+	echo "install client script..." . PHP_EOL;
 	file_put_contents( $client_script, $client_script_content );
 
 	// add simple support for CGI/FastCGI
 	// @see https://support.tigertech.net/php-http-auth
+	echo "install .htaccess..." . PHP_EOL;
 	file_put_contents( SWIM_CLIENT_PATH . '/.htaccess', 'CGIPassAuth On' );
 }
 
 // house keeping
 if ( file_exists( $public_html_dir . '/.swim-client' ) ) {
+	echo "found old installation, clean..." . PHP_EOL;
 	@unlink( $public_html_dir . '/.swim-client' );
 }
